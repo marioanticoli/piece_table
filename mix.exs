3c5a75e0571887b0cc08ba9842c95e0267cba523
Mario Anticoli <mario.anticoli@fcm.digital>
2023-05-22 09:20:57 +0200

    make it a hex package

--- mix.exs
+++ mix.exs
@@ -9,11 +9,14 @@ defmodule PieceTable.MixProject do
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       # Treat warnings as error
-      elixirc_options: [warnings_as_errors: true]
+      elixirc_options: [warnings_as_errors: true],
       # TODO: check usage in CI
       # dialyzer: [
       # plt_file: {:no_warn, "priv/plts/dialyzer.plt"}
       # ]
+      description: description(),
+      package: package(),
+      source_url: "https://github.com/marioanticoli/piece_table"
     ]
   end
 
@@ -27,6 +30,20 @@ defmodule PieceTable.MixProject do
     [
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.3", only: [:dev], runtime: false}
+    ]
+  end
+
+  defp description() do
+    "A piece-table data structure."
+  end
+
+  defp package() do
+    [
+      name: "piece_table",
+      files:
+        ~w(lib priv .formatter.exs mix.exs README* readme* LICENSE* license* CHANGELOG* changelog*),
+      licenses: ["MIT License"],
+      links: %{"Github" => "https://github.com/marioanticoli/piece_table"}
     ]
   end
 end
